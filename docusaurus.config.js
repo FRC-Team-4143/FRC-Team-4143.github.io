2ce0b5b1e33c9e61e4bc150370b8c0c804fef774
ColeHunt <huntcr2001@gmail.com>
2025-07-28 19:45:37 -0500

    formatting and adds app imgs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'MARS/WARS Docs',
   tagline: 'Documentation for the MARS/WARS robotics team',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
